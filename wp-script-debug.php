8ac8031b14216cd40dcfcef8452cf0922b91812c
gantsta <gantsta@users.noreply.github.com>
2018-11-12 11:03:47 +0000

    Initial commit

---
+++ wp-script-debug.php
@@ -0,0 +1,25 @@
+/**
+ * Debugging script for dumping an array of the registered scripts and stylesheets
+ * Note: You can change 'handle' for 'src' to view a scripts' filename
+ * 
+ * @since		1.0.0
+ */
+function print_scripts_styles() {
+	$result = [];
+	$result['scripts'] = [];
+	$result['styles'] = [];
+
+	// Print all loaded Scripts (JS)
+	global $wp_scripts;
+	foreach( $wp_scripts->queue as $script ) :
+		$result['scripts'][] =  $wp_scripts->registered[$script]->handle . ";";
+	endforeach;
+
+	// Print all loaded Styles (CSS)
+	global $wp_styles;
+	foreach( $wp_styles->queue as $style ) :
+		$result['styles'][] =  $wp_styles->registered[$style]->handle . ";";
+	endforeach;
+
+	print_r($result);
+}
